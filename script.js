f7082bbe4bc8b88cf76297dacc1d445cbd14a5ac
n-c0de-r <s0577683@htw-berlin.de>
2021-12-19 22:36:44 +0100

    new functions, better menu

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // Not much here. Give me some time. I'm working on it, I promise :)
+function fillNavbar(){
+    let navbar = document.getElementById("collapsableNavbar");
+    navbar.innerHTML = "<a href=\"../index.html\" id=\"home\">Home</a>\n"
+    + "<a href=\"../subpages/211217_API_project.html\" id=\"audio\">Audio API</a>\n"
+    + "<a href=\"#\" id=\"zelda\">Zelda API</a>\n"
+    + "<a href=\"../subpages/211217_TicTacToe_project.html\" id=\"ttt\">Tic Tac Toe</a>\n"
+    + "<a href=\"../subpages/211217_StopTheVirus_project.html\" id=\"virus\">Stop The Virus</a>\n";
+}
